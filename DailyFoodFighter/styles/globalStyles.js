29c2d81c983f47c0d180f2151851e4d081bb0780
rjswn97 <gunjudavid@naver.com>
2022-03-26 13:35:31 -0400

    added CSS styling

--- DailyFoodFighter/styles/globalStyles.js
+++ DailyFoodFighter/styles/globalStyles.js
@@ -14,3 +14,18 @@ export const globalStyles = StyleSheet.create({
         width: '100%',
     },
 })
+
+export const homeStyle = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homeButton: {
+        padding: 10,
+        position: 'relative',
+        marginTop: '20%',
+        width: '100%',
+    },
+})
