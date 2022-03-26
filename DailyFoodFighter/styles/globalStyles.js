c95a2b1d503e523732034b818cf83ef2bbc32344
rjswn97 <gunjudavid@naver.com>
2022-03-26 13:41:07 -0400

    added CSS styling

--- DailyFoodFighter/styles/globalStyles.js
+++ DailyFoodFighter/styles/globalStyles.js
@@ -15,6 +15,57 @@ export const globalStyles = StyleSheet.create({
     },
 })
 
+export const ingridientStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    header: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center',
+        backgroundColor: '#ADD8E6',
+    },
+
+    blank: {
+        flex: 10,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    protein: {
+        fontWeight: 'bold',
+        fontSize: 14,
+    },
+    vege: {
+        fontWeight: 'bold',
+        fontSize: 14,
+
+    },
+    buttonDesign: {
+
+        backgroundColor: "black",
+    },
+
+    bottom: {
+        padding: 10,
+        position: 'relative',
+        marginTop: '30%',
+        width: '100%',
+    },
+    buttonText: {
+        color: 'white',
+        alignItems: 'center',
+        fontSize: 18,
+    }
+
+})
+
 export const homeStyle = StyleSheet.create({
     container: {
         flex: 1,
