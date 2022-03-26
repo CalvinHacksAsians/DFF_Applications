159698b82305cd3c48081b836efe9b61849fec0a
Kun Kang <kunkang@dhcp85-51.calvin.edu>
2022-03-26 09:34:22 -0400

    connected listing recipes to db

--- DailyFoodFighter/screens/listRecipe.js
+++ DailyFoodFighter/screens/listRecipe.js
@@ -1,23 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Alert, Button, FlatList, Linking, StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-export default function RecipeListScreen({ navigation }) {
+export default function RecipeListScreen({ route, navigation }) {
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+  
+    const getRecipes = async () => {
+       try {
+        const response = await fetch('https://raw.githubusercontent.com/CalvinHacksAsians/DFF_Database/main/recipedb.json');
+        const json = await response.json();
+        setData(json.recipes);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  
+    useEffect(() => {
+      getRecipes();
+    }, []);
+  
     return (
-        <View style={styles.container}>
-            <Text>This is where the recipes are listed</Text>
-            <Button title="select recipe" onPress={() => {
-                navigation.navigate('Recipe');
-            }} />
-            <StatusBar style="auto" />
-        </View>
+      <View style={{ flex: 1, padding: 24 }}>
+        {isLoading ? <ActivityIndicator/> : (
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <Text onPress={() =>{
+                  Alert.alert(
+                      "Press 'OK' to go to source.",
+                      item.recipeInfo.source,
+                      [
+                          {
+                              text: "OK",
+                              onPress: () => {
+                                Linking.openURL(item.recipeInfo.source)
+                              }
+                          }
+                      ]
+                  )
+                // alert(item.recipeInfo.source);
+              }}> {item.recipeInfo.name}</Text>
+            )}
+          />
+        )}
+      </View>
     );
-}
+  };
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
     },
-  });
+});
---
+++ DailyFoodFighter/styles/globalStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    homeButton: {
+        padding: 15,
+        position: 'relative',
+        marginTop: '95%',
+        width: '100%',
+    },
+})
